c767fc62f79772ef268adaa4c3dd41e1ab411fa6
Manny Negussie <mannyalx1@gmail.com>
2022-04-08 14:18:26 -0700

    update bonus 102 #8

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,51 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
- * main - main block
- * Description: Write a program that prints all possible
- * different combinations of two digits
- * Return: 0
+ * main - Prints all possible combinations of a pair of
+ * two digit numbers, without any repetition.
+(*
+ * Return: 0 on success.
  */
+
 int main(void)
 {
-int c;
-int d;
-int e;
-int f = 0;
+	int i, j;
 
-while (f < 10)
-{
-	e = 0;
-	while (e < 9)
+	for (i = 0; i <= 99; i++)
 	{
-		d = 0;
-		while (d < 10)
+		for (j = 0; j <= 99; j++)
 		{
-			c = 0;
-			while (c < 10)
+			if (i < j && i != j)
 			{
-				if (!(f == 0 && e == 0 && d == 0 && c == 0) && !(f == 0 && e == 1 && d == 0 && c == 0)
-				&& !(f == 0 && e == 1 && d == 0 && c == 1))
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 98 || j != 99)
 				{
-					putchar('0' + f);
-					putchar('0' + e);
+					putchar(',');
 					putchar(' ');
-					putchar('0' + d);
-					putchar('0' + c);
-					if (!(f + e == 17 && c + d == 18 && d == 9))
-					{
-						putchar(',');
-						putchar(' ');
-					}
 				}
-				c++;
 			}
-			d++;
 		}
-		e++;
 	}
-	f++;
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
