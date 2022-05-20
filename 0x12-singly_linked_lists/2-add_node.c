367564f7d02dfec30807397bed134104cb43a2c5
Manny Negussie <102585213+MannyNe2@users.noreply.github.com>
2022-05-20 18:02:25 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,30 +1,38 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
+#include <string.h>
 
 /**
- * add_node - adds a new node at the beginning of a linked list
- * @head: double pointer to the list_t list
- * @str: new string to add in the node
- *
- * Return: the address of the new element, or NULL if it fails
+ * *add_node - adds a node to a linked list head
+ * @head: linked list head
+ * @str: string element of the linked list
+ * Return: number of elements
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
-	unsigned int len = 0;
+	list_t *new_node;
+	int len_str;
 
-	while (str[len])
-		len++;
+	new_node = (list_t *) malloc(sizeof(list_t));
 
-	new = malloc(sizeof(list_t));
-	if (!new)
+	if (new_node == NULL)
+	{
+		free(new_node);
 		return (NULL);
+	}
 
-	new->str = strdup(str);
-	new->len = len;
-	new->next = (*head);
-	(*head) = new;
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	for (len_str = 0; str[len_str] != '\0';  len_str++)
+	;
 
-	return (*head);
+	new_node->len = len_str;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
